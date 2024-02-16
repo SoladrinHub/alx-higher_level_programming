695287dac02bd96eb77307ef188cc8d8830281d2
SoladrinHub <cointoken8021@gmail.com>
2024-02-16 03:27:51 -0800

    Write a script that lists all databases of your MySQL server.

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,8 +1,8 @@
--- Write a script that lists all databases of your MySQL server.
+-- Write a script that lists all databases of your MySQL server
+Enter password:
 Database
 hbtn_0c_0
 information_schema
 mysql
 performance_schema
 sys
-
